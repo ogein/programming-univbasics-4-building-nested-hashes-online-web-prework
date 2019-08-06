f6dcfc6ebb1e73b636f9508164717021720e1038
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-06 04:23:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,7 @@ end
 
 def monopoly_with_second_tier
   hash = {
-    :railroads => {pieces: "", rent_in_dollars: {}, names: {}}
+    :railroads => {pieces: 4, rent_in_dollars: {}, names: {}}
   }
 end
 
